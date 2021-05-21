38cefa1f9884462c2952176743026ae51db66dd3
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-05-21 17:18:04 +0100

    added cors

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 
 const htmlRouter = require("./routes/htmlroutes");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.static("public"));
 app.use("/notes", htmlRouter);
-app.use("/", htmlRouter);
+app.use("*", htmlRouter);
 
 app.listen(PORT, () => {
   console.log(`Navigate to http://localhost:${PORT}`);
