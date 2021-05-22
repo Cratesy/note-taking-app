8ef2938b008b294fb1efebe04d6d73f674166ea3
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-05-22 15:10:51 +0100

    added middleware

--- server/index.js
+++ server/index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const cors = require("cors");
 
 const htmlRouter = require("./routes/htmlroutes");
-// const apiRouter = require("./routes/apiRoutes");
+const apiRouter = require("./routes/apiRoutes");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
 app.use(express.static("public"));
-// app.use("/api", apiRouter);
+app.use("/api", apiRouter);
 app.use("/", htmlRouter);
 
 app.listen(PORT, () => {
