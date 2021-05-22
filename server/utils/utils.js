9775c2027df59767d63b7d005de894af531b3b48
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-05-22 15:09:53 +0100

    added utils functions

---
+++ server/utils/utils.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+const readFromFile = (fileName) => {
+  try {
+    const data = fs.readFileSync(
+      path.join(__dirname, `../db/${fileName}.json`),
+      "utf-8"
+    );
+    return JSON.parse(data);
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+
+const writeToFile = (fileName, data) => {
+  try {
+    fs.writeFileSync(path.join(__dirname, `../db/${fileName}.json`), data);
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+module.exports = { readFromFile, writeToFile };
