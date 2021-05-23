58e29fc47671c799fb979bd6058458ff5a646794
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-05-23 15:24:39 +0100

    update html controllers

--- server/controllers/htmlcontrollers.js
+++ server/controllers/htmlcontrollers.js
@@ -1,11 +1,11 @@
 const path = require("path");
-// render landing page
+
 const renderIndexPage = (req, res) => {
   const filePath = path.join(__dirname, "../../public/index.html");
 
   res.sendFile(filePath);
 };
-// render notes page
+
 const renderNotesPage = (req, res) => {
   const filePath = path.join(__dirname, "../../public/notes.html");
 
